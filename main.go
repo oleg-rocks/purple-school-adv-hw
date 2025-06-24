605523e4cfa02e8667884a5374e540ebefaa9a2e
Oleg Kirsanov <olegkirsanov22@gmail.com>
2025-06-24 23:27:39 +0300

    Solution for concurrency hw 1

--- main.go
+++ main.go
@@ -1,4 +1,38 @@
 package main
 
+import (
+	"fmt"
+	"math"
+	"math/rand"
+)
+
 func main() {
+	numCh := make(chan []int)
+	sqrCh := make(chan []int)
+	go generateNum(numCh)
+	go squareNum(numCh, sqrCh)
+	result := <-sqrCh
+	fmt.Println(result)
+}
+
+func generateNum(out chan []int) {
+	var randSlice []int
+	for i := 0; i < 10; i++ {
+		num := randNum()
+		randSlice = append(randSlice, num)
+	}
+	out <- randSlice
+}
+
+func squareNum(in chan []int, out chan []int) {
+	var sqrSlice []int
+	for _, val := range <-in {
+		sqrNum := int(math.Pow(float64(val), 2))
+		sqrSlice = append(sqrSlice, sqrNum)
+	}
+	out <- sqrSlice
+}
+
+func randNum() int {
+	return rand.Intn(101)
 }
