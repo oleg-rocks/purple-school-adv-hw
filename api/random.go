72b415aaedcf46d41235fc55824623a0a701684b
Oleg Kirsanov <olegkirsanov22@gmail.com>
2025-06-29 10:53:25 +0300

    Code fix 1

--- api/random.go
+++ api/random.go
@@ -4,18 +4,23 @@ import (
 	"math/rand"
 	"net/http"
 	"strconv"
+	"time"
 )
 
-type DiceNumberGenerator struct{}
+type DiceNumberGenerator struct{
+	Generator *rand.Rand
+}
 
 func NewDiceNumberGenerator(router *http.ServeMux) {
-	handler := &DiceNumberGenerator{}
+	handler := &DiceNumberGenerator{
+		Generator: rand.New(rand.NewSource(time.Now().UnixNano())),
+	}
 	router.HandleFunc("/dice", handler.Generate())
 }
 
 func (d *DiceNumberGenerator) Generate() http.HandlerFunc {
 	return func(w http.ResponseWriter, req *http.Request) {
-		res := strconv.Itoa(rand.Intn(6) + 1)
+		res := strconv.Itoa(d.Generator.Intn(6) + 1)
 		w.Write([]byte(res))
 	}
 }
